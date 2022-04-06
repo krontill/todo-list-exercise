2014d43681c9b6e28ccfb8c6795787351696ceca
krontill <till.work@gmail.com>
2022-04-06 15:03:15 +0200

    Add normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
 import { Wrapper } from './App.styled';
+import 'normalize.css';
 
 export const App = () => <Wrapper>list</Wrapper>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'assets/index.html',
-    }),
-    new ESLintPlugin({
-      extensions: ['.tsx', '.ts'],
-    }),
-  ],
+module.exports = (env, argv) => {
+  const devMode = argv.mode === 'development';
+
+  return {
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'assets/index.html',
+      }),
+      new ESLintPlugin({
+        extensions: ['.tsx', '.ts'],
+      }),
+    ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+    optimization: {
+      minimizer: [new CssMinimizerPlugin(), '...'],
+    },
+  };
 };
