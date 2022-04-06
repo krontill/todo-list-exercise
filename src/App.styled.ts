f2da17cba8992290a601cb237f675958d9efa369
krontill <till.work@gmail.com>
2022-04-06 13:27:42 +0200

    Add react and styled components

---
+++ src/App.styled.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  border-bottom: 1px solid;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Wrapper } from './App.styled';
+
+export const App = () => (
+  <Wrapper>list</Wrapper>
+);
